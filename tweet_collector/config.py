1baebac73fc6ccb739ea45a0cbb70ec7149cd3c5
Daniil Zhilyayev <63920683+JoexTitan@users.noreply.github.com>
2022-12-20 12:29:48 -0500

    Update config.py

--- tweet_collector/config.py
+++ tweet_collector/config.py
@@ -1,6 +1,6 @@
 cfg = {
-    "CONSUMER_API_KEY"    : "...",
-    "CONSUMER_API_SECRET" : "...",
-    "ACCESS_TOKEN"        : "...",
-    "ACCESS_TOKEN_SECRET" : "..."
+    "CONSUMER_API_KEY"    : "***unique_CONSUMER_API_KEY_goes_here***",
+    "CONSUMER_API_SECRET" : "***unique_CONSUMER_API_SECRET_goes_here***",
+    "ACCESS_TOKEN"        : "***unique_ACCESS_TOKEN_goes_here***",
+    "ACCESS_TOKEN_SECRET" :"***unique_ACCESS_TOKEN_SECRET_goes_here***"
     }
